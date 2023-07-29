527383ff1301fba0c958b375d0eb1d850a7e1d84
hxkim10 <abdelhakimtiabi@gmail.com>
2023-07-29 16:07:26 +0100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// 1
+const num1 = 31,
+  num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+// 2
+let random;
+random = Math.ceil(Math.random() * 100);
+
+//3
+const num3 = 4,
+  num4 = 7;
+const mod = num3 % num4;
+
+//4
+const max = Math.max(14, 15, 20);
+console.log(max);
